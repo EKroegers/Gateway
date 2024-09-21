e9bf192d78ae1941b70443a418a8ff2f25700923
EKroegers <kroegerethan@gmail.com>
2024-09-20 22:42:56 -0400

    Added conditional logic for non-draggable frames

--- src/Components/Frame/Frame.tsx
+++ src/Components/Frame/Frame.tsx
@@ -5,9 +5,9 @@ import makeDraggable from '../../utils/makeDraggable';
 import { Props } from './Frame.types';
 import './Frame.css';
 
-const Frame = ({ children }: Props) => {
+const Frame = ({ children, isDraggable }: Props) => {
   const [minimized, setMinimized] = useState(false);
-  return makeDraggable(
+  const frameComponentBase = (
     <div
       data-testid="frame"
       id={`Gateway-Frame-Container ${minimized ? 'Minimized' : ''}`}
@@ -19,9 +19,13 @@ const Frame = ({ children }: Props) => {
         id="Gateway-Frame-Close-Button"
       />
       <div id="Gateway-Frame">{children}</div>
-    </div>,
-    'Gateway-Frame-Container'
+    </div>
   );
+  if (isDraggable) {
+    return makeDraggable(frameComponentBase, 'Gateway-Frame-Container');
+  } else {
+    return frameComponentBase;
+  }
 };
 
 export default Frame;
--- src/Components/Frame/Frame.types.ts
+++ src/Components/Frame/Frame.types.ts
@@ -3,4 +3,6 @@ import React from 'react';
 export interface Props {
   // JSX components to be passed to the interior of the draggable Frame
   children: React.ReactNode;
+  // Boolean determining if the frame should be draggable
+  isDraggable: boolean;
 }
